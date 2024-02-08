2c3bc4bed38f761962fb31aa9072841ee4ec3382
sudip.mondal3@cognizant.com <sudip.mondal3@cognizant.com>
2024-02-08 11:44:27 +0000

    Test

--- Dev/5 Automating actions in revision control.sql
+++ Dev/5 Automating actions in revision control.sql
@@ -172,3 +172,7 @@
 -- MAGIC Apache, Apache Spark, Spark and the Spark logo are trademarks of the <a href="https://www.apache.org/">Apache Software Foundation</a>.<br/>
 -- MAGIC <br/>
 -- MAGIC <a href="https://databricks.com/privacy-policy">Privacy Policy</a> | <a href="https://databricks.com/terms-of-use">Terms of Use</a> | <a href="https://help.databricks.com/">Support</a>
+
+-- COMMAND ----------
+
+Test
